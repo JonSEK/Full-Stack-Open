211f5121701449555eaf5b977ac40f91b401fec5
JonSEK <engkien@ymail.com>
2023-12-07 09:15:54 +0800

    Exercise: Data for Countries

--- Part 2/Data For Countries/src/App.jsx
+++ Part 2/Data For Countries/src/App.jsx
@@ -1,38 +1,37 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
+
+const api_key = import.meta.env.VITE_API_KEY;
 const baseUrl = "https://studies.cs.helsinki.fi/restcountries/api/all";
+const weatherApiUrl = "https://api.openweathermap.org/data/2.5/weather";
 
 const App = () => {
   const [countries, setCountries] = useState([]);
-  const [filteredCountries, setFilteredCountries] = useState([]);
-  const [newFilter, setNewFilter] = useState("");
+  const [selectedCountry, setSelectedCountry] = useState(null);
   const [loading, setLoading] = useState(true);
   const [queryPrompt, setQueryPrompt] = useState(false);
-  const [selectedCountry, setSelectedCountry] = useState(null);
+  const [newFilter, setNewFilter] = useState("");
 
   const handleFilter = (event) => {
     const filterValue = event.target.value.toLowerCase();
     setNewFilter(filterValue);
 
-    const filtered = countries.filter((country) =>
-      country.name.common.toLowerCase().includes(filterValue)
-    );
+    if (filterValue.length > 0) {
+      const filtered = countries.filter((country) =>
+        country.name.common.toLowerCase().includes(filterValue)
+      );
 
-    setFilteredCountries(filtered);
+      if (filtered.length > 10) {
+        setQueryPrompt(true);
+      } else {
+        setQueryPrompt(false);
+      }
 
-    if (filtered.length > 10) {
-      setQueryPrompt(true);
+      setSelectedCountry(filtered.length === 1 ? filtered[0] : null);
     } else {
       setQueryPrompt(false);
+      setSelectedCountry(null);
     }
-    setSelectedCountry(null);
-    if (filtered.length === 1) {
-      setSelectedCountry(filtered[0]);
-    }
-  };
-
-  const handleShowDetails = (country) => {
-    setSelectedCountry(country);
   };
 
   useEffect(() => {
@@ -40,12 +39,43 @@ const App = () => {
       .get(baseUrl)
       .then((response) => {
         setCountries(response.data);
-        setFilteredCountries(response.data);
       })
       .finally(() => {
         setLoading(false);
       });
   }, []);
+
+  useEffect(() => {
+    const fetchWeather = async () => {
+      if (selectedCountry) {
+        try {
+          const weatherResponse = await axios.get(weatherApiUrl, {
+            params: {
+              q: `${
+                selectedCountry.capital
+              },${selectedCountry.cca2.toLowerCase()}`,
+              appid: api_key,
+              units: "metric",
+            },
+          });
+
+          setSelectedCountry((prevCountry) => ({
+            ...prevCountry,
+            weather: {
+              temperature: weatherResponse.data.main.temp,
+              description: weatherResponse.data.weather[0].description,
+              icon: weatherResponse.data.weather[0].icon,
+              wind: weatherResponse.data.wind.speed,
+            },
+          }));
+        } catch (error) {
+          console.error("Error fetching weather data:", error);
+        }
+      }
+    };
+
+    fetchWeather();
+  }, [selectedCountry]);
 
   return (
     <div>
@@ -61,19 +91,21 @@ const App = () => {
                 <p>Too many matches, specify another filter</p>
               ) : (
                 <>
-                  {filteredCountries.length > 0 ? (
+                  {countries.length > 0 && (
                     <ul>
-                      {filteredCountries.map((country) => (
-                        <li key={country.cca3}>
-                          {country.name.common}
-                          <button onClick={() => handleShowDetails(country)}>
-                            Show Details
-                          </button>
-                        </li>
-                      ))}
+                      {countries
+                        .filter((country) =>
+                          country.name.common.toLowerCase().includes(newFilter)
+                        )
+                        .map((country) => (
+                          <li key={country.ccn3}>
+                            {country.name.common}
+                            <button onClick={() => setSelectedCountry(country)}>
+                              Show Details
+                            </button>
+                          </li>
+                        ))}
                     </ul>
-                  ) : (
-                    <p>No matching countries found.</p>
                   )}
                   {selectedCountry && (
                     <div>
@@ -92,6 +124,24 @@ const App = () => {
                         src={selectedCountry.flags.png}
                         alt={`${selectedCountry.name.common} Flag`}
                       />
+                      {selectedCountry.weather && (
+                        <div>
+                          <h3>
+                            Weather Information for {selectedCountry.capital}
+                          </h3>
+                          <p>
+                            Temperature: {selectedCountry.weather.temperature}°C
+                          </p>
+                          <p>
+                            Description: {selectedCountry.weather.description}
+                          </p>
+                          <img
+                            src={`http://openweathermap.org/img/wn/${selectedCountry.weather.icon}.png`}
+                            alt="Weather Icon"
+                          />
+                          <p>Wind Speed: {selectedCountry.weather.wind}m/s</p>
+                        </div>
+                      )}
                     </div>
                   )}
                 </>
