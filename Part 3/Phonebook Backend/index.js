110d73585993cbaf011de4e5ff448ceb0d0d48d7
JonSEK <engkien@ymail.com>
2023-12-13 11:13:12 +0800

    Exercises 3.1.-3.8.

--- Part 3/Phonebook Backend/index.js
+++ Part 3/Phonebook Backend/index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const app = express();
 const morgan = require("morgan");
+const cors = require("cors");
 
 app.use(express.json());
+app.use(cors());
 
 morgan.token("requestData", (req) => {
   const { name, number } = req.body;
@@ -94,7 +96,7 @@ app.post(
   }
 );
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
